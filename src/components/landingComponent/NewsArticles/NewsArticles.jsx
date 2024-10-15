e805a12572c719a824f7b439b49c30a3f8bd371b
esmail <you@example.com>
2024-10-15 12:35:34 +0330

    NewsArticlesStyleHover

--- src/components/landingComponent/NewsArticles/NewsArticles.jsx
+++ src/components/landingComponent/NewsArticles/NewsArticles.jsx
@@ -13,35 +13,69 @@ const NewsArticles = () => {
             </div>
             <div className="subtitle"> !خودت را با خبر کن</div>
 
-            {/* گالری تصاویر */}
-            <div className="gallery">
-                <div className="square">
-                    <img src='/images/Rectangle 37.png' alt="تصویر 1"></img>
-                    <div className="image-caption">  دنیای آنلاین؛ راهی برای افزایش فروش</div>
-                </div>
-                <div className="square">
-                    <img src='/images/Rectangle 38.png' alt="تصویر 2"></img>
-                    <div className="image-caption">  بازاریابی حسی راهی ساده و کم هزینه 
-                    برای افزایش فروش</div>
-                </div>
-                <div className="rectangle">
-                    <img src='/images/Rectangle 41.png' alt="تصویر 3" className='rectangle-images'></img>
-                    <div className="image-caption">  از کجا مشتری پیدا کنم؟ راه‌های پیدا کردن مشتری</div>
-                </div>
-                <div className="rectangle">
-                    <img src='/images/Rectangle 40.png' alt="تصویر 4" className='rectangle-images'></img>
-                    <div className="image-caption">  نیازهای مشتری را بشناسید تا درآمدتان بیشتر شود</div>
-                </div>
-                <div className="square mt-custom">
-                    <img src='/images/Rectangle 42.png' alt="تصویر 5"></img>
-                    <div className="image-caption">  ورود به دنیای آنلاین؛ راهی مطمئن برای
-                    افزایش فروش</div>
-                </div>  
-                <div className="square mt-custom">
-                    <img src='/images/Rectangle 43.png' alt="تصویر 6"></img>
-                    <div className="image-caption">  جذب مشتری با تبلیغات دهان به دهان</div>
-                </div> 
-            </div>
+           {/* گالری تصاویر */}
+<div className="gallery">
+  <div className="square">
+    <div className="image-container">
+      <img src='/images/Rectangle 37.png' alt="تصویر 1" />
+      <div className="overlay">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">دنیای آنلاین؛ راهی برای افزایش فروش</div>
+  </div>
+
+  <div className="square">
+    <div className="image-container">
+      <img src='/images/Rectangle 38.png' alt="تصویر 2" />
+      <div className="overlay">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">بازاریابی حسی راهی ساده و کم هزینه برای افزایش فروش</div>
+  </div>
+
+  <div className="rectangle">
+    <div className="image-container">
+      <img src='/images/Rectangle 41.png' alt="تصویر 3" className='rectangle-images' />
+      <div className="overlay-rectangle">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">از کجا مشتری پیدا کنم؟ راه‌های پیدا کردن مشتری</div>
+  </div>
+
+  <div className="rectangle">
+    <div className="image-container">
+      <img src='/images/Rectangle 40.png' alt="تصویر 4" className='rectangle-images' />
+      <div className="overlay-rectangle">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">نیازهای مشتری را بشناسید تا درآمدتان بیشتر شود</div>
+  </div>
+
+  <div className="square mt-custom">
+    <div className="image-container">
+      <img src='/images/Rectangle 42.png' alt="تصویر 5" />
+      <div className="overlay">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">ورود به دنیای آنلاین؛ راهی مطمئن برای افزایش فروش</div>
+  </div>  
+
+  <div className="square mt-custom">
+    <div className="image-container">
+      <img src='/images/Rectangle 43.png' alt="تصویر 6" />
+      <div className="overlay">
+        <button className="more-info-btn">مطالعه مطلب</button>
+      </div>
+    </div>
+    <div className="image-caption">جذب مشتری با تبلیغات دهان به دهان</div>
+  </div>
+</div>
+
 
             {/* دکمه مشاهده بیشتر */}
             <div className="button-section mt-custom">
