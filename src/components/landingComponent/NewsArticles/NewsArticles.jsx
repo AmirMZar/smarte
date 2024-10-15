cd0d040bc65788d16f282f1fa2607fcf92284221
esmail <you@example.com>
2024-10-15 09:19:20 +0330

    NewsArticlesImages

--- src/components/landingComponent/NewsArticles/NewsArticles.jsx
+++ src/components/landingComponent/NewsArticles/NewsArticles.jsx
@@ -16,31 +16,38 @@ const NewsArticles = () => {
             {/* گالری تصاویر */}
             <div className="gallery">
                 <div className="square">
-                    <img src='/images/Rectangle 37.png'></img>
+                    <img src='/images/Rectangle 37.png' alt="تصویر 1"></img>
+                    <div className="image-caption">  دنیای آنلاین؛ راهی برای افزایش فروش</div>
                 </div>
                 <div className="square">
-                <img src='/images/Rectangle 38.png'></img>
+                    <img src='/images/Rectangle 38.png' alt="تصویر 2"></img>
+                    <div className="image-caption">  بازاریابی حسی راهی ساده و کم هزینه 
+                    برای افزایش فروش</div>
                 </div>
                 <div className="rectangle">
-                <img src='/images/Rectangle 41.png'></img>
+                    <img src='/images/Rectangle 41.png' alt="تصویر 3" className='rectangle-images'></img>
+                    <div className="image-caption">  از کجا مشتری پیدا کنم؟ راه‌های پیدا کردن مشتری</div>
                 </div>
                 <div className="rectangle">
-                <img src='/images/Rectangle 40.png'></img>
+                    <img src='/images/Rectangle 40.png' alt="تصویر 4" className='rectangle-images'></img>
+                    <div className="image-caption">  نیازهای مشتری را بشناسید تا درآمدتان بیشتر شود</div>
                 </div>
                 <div className="square mt-custom">
-                <img src='/images/Rectangle 42.png'></img>
+                    <img src='/images/Rectangle 42.png' alt="تصویر 5"></img>
+                    <div className="image-caption">  ورود به دنیای آنلاین؛ راهی مطمئن برای
+                    افزایش فروش</div>
                 </div>  
                 <div className="square mt-custom">
-                <img src='/images/Rectangle 43.png'></img>
+                    <img src='/images/Rectangle 43.png' alt="تصویر 6"></img>
+                    <div className="image-caption">  جذب مشتری با تبلیغات دهان به دهان</div>
                 </div> 
             </div>
 
-          {/* دکمه مشاهده بیشتر */}
+            {/* دکمه مشاهده بیشتر */}
             <div className="button-section mt-custom">
                 <button className="view-more-btn">
-                <span className="arrow"> &lt; </span> {/* فلش > */}
-
-                         مشاهده بیشتر
+                    <span className="arrow"> &lt; </span> {/* فلش > */}
+                    مشاهده بیشتر
                 </button>
             </div>
         </div>
