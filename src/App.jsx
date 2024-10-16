dae61afa37c1937338bb64d6b87c07ce52ac1a9d
esmail <you@example.com>
2024-10-16 14:15:48 +0330

    Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 
-import './App.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Landing from './Screen/landing/landing'
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './Screen/landing/landing';
+import Layout from './components/Layout/Layout';
 
 function App() {
-
   const router = createBrowserRouter([
-    {path:'/'  , element:<Landing/>}
-  ])
-
+    {path: '/',element: (<Layout> <Landing /> </Layout>),},]);
   return (
     <>
-    <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
--- src/Screen/landing/landing.jsx
+++ src/Screen/landing/landing.jsx
@@ -1,13 +1,9 @@
 import NewsArticles from "../../components/landingComponent/NewsArticles/NewsArticles"
-import Header from "../../components/common/Header/Header"
 import Podcast from "../../components/landingComponent/Podcast/Podcast"
-import Footer from "../../components/common/Footer/Footer"
 const Landing = () => {
     return ( <>
-        <Header />
         <NewsArticles/>
         <Podcast/>
-        <Footer/>
     </> );
 }
  
